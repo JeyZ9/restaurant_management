5a149d52032d6e75870bccc3eda3f98ed3ebf229
JeyZ9 <ggjj4511@gmail.com>
2025-06-20 00:12:02 +0700

    Move validate input field from method register and login to ExceptionControllerAdvice

--- src/main/java/com/app/restaurant_management/controllers/UserController.java
+++ src/main/java/com/app/restaurant_management/controllers/UserController.java
@@ -13,28 +13,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.View;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(PathConstants.AUTH)
 public class UserController {
 
     private final UserServiceImpl userService;
-    private final View error;
 
     @Autowired
-    public UserController(UserServiceImpl userService, View error){
+    public UserController(UserServiceImpl userService){
         this.userService = userService;
-        this.error = error;
     }
 
 //    @PostMapping(path = {"/login", "/signin"})
